a4655bb11ff5adc0beefbb0699871fbc8e577e7a
yamauz <yamauzy@gmail.com>
2021-01-15 23:58:26 +0900

    Add locale switch button.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,27 @@
-import Link from "next/link";
 import { useRouter } from "next/router";
 import { NextPage } from "next";
-import { Box } from "@chakra-ui/react";
+import { Text, Button } from "@chakra-ui/react";
 
 const Page: NextPage = () => {
   const router = useRouter();
   const { locale } = router;
 
   const WELCOME = locale === "ja" ? "ようこそ" : "Welcome";
+  const _locale = locale === "ja" ? "en" : "ja";
 
   return (
-    <Box
-      m={2}
-      n={2}
-    >
-      {WELCOME}
-    </Box>
+    <>
+      <Text m={2}>{WELCOME}</Text>
+
+      <Button
+        colorScheme={"teal"}
+        onClick={() => {
+          router.push("/", "/", { locale: _locale });
+        }}
+      >
+        Switch Locale
+      </Button>
+    </>
   );
 };
 
